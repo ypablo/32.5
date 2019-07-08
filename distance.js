c1b75b74e6742053f9591511e30916f8394692a0
ypablo <yfedora@gmail.com>
2019-07-08 21:32:42 +0100

    32.5 Testy jednostkowe

--- distance.js
+++ distance.js
@@ -1,6 +1,24 @@
 // calculateDistancePoints.js
 const calculateDistancePoints = (distance, hillSize, kPoint) => {
-    ...
-  };
-  
-  module.exports = calculateDistancePoints;
+  let distance;
+  let hillSize;
+  let kPoint;
+  let skiJumperPts;
+  let Pts;
+
+  if (hillSize == "normal") {
+    Pts = (Math.round(distance * 2) / 2 - kPoint) * 2;
+    skiJumperPts = 60 + Pts;
+  } else if (hillSize == "medium") {
+    Pts = (Math.round(distance * 2) / 2 - kPoint) * 1.8;
+    skiJumperPts = 60 + Pts;
+  } else if (hillSize == "large") {
+    Pts = (Math.round(distance * 2) / 2 - kPoint) * 1.2;
+    skiJumperPts = 120 + Pts;
+  }
+
+  return (Math.round(skiJumperPts * 2) / 2).toFixed(1);
+
+};
+
+module.exports = calculateDistancePoints;
--- style.js
+++ style.js
@@ -1,6 +1,18 @@
 // calculateStylePoints.js
 const calculateStylePoints = (styleNotes) => {
-    ...
-  };
-  
-  module.exports = calculateStylePoints;
+  let styleNotes = [];
+
+  if (styleNotes.length != 5) {
+    alert("Incorrect number of notes. Please try again.");
+    break;
+  } else {
+    let minNote = Math.min(...styleNotes);
+    let maxNote = Math.max(...styleNotes);
+    let sumNote = styleNotes.reduce((a, b) => a + b, 0);
+    let finalNote = sumNote - minNote - maxNote;
+
+  }
+
+};
+
+module.exports = calculateStylePoints;
